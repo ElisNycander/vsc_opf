5d064f148968c68ca692cfbad866edb935c659b2
Elis Nycander <elisn@UG.KTH.SE>
2017-10-26 10:09:52 +0200

    Added output to file

--- check_opf_options.m
+++ check_opf_options.m
@@ -1,10 +1,14 @@
-function check_opf_options(optns)
+function optns = check_opf_options(optns)
 
 
 assert(isempty(setdiff(optns.gen.maxPg,optns.gen.fixPg)), ...
     'Error: ''maxPg'' must be subset of ''fixPg''' );
 
-
+if ~isempty(optns.outputFile)   
+    optns.fileID = fopen(optns.outputFile,'w');
+else
+    optns.fileID = '';
+end
 %optns.mpopt.opf.violation = optns.foptions.TolCon;
 
 
--- run_opf_case9.m
+++ run_opf_case9.m
@@ -6,20 +6,24 @@ vscopf_define_constants;
 %% OPTIONS
 optns = struct();
 
+optns.outputFile = '';
 
-optns.case = 'case9';
-optns.contingencies = 'case9_contingencies';
-optns.stabilityMargin = 0;
+optns.caseFile = 'case9';
+optns.contingencyFile = 'case9_contingencies';
+optns.stabilityMargin = 0.0;
 
 %% generator options
 optns.gen.fixPg = [1]; % generators which is fixed for stressed cases
+optns.gen.fixQg = [];
+
 optns.gen.maxPg = [1]; % generators for which to max production (must be fixed)
 optns.gen.maxPgLim = [3000];
 
 optns.branch.limit = 0; % turn on/off branch limits 
 optns.branch.rateA = []; % branch limits
 
-optns.load.loadIncreaseArea = 1; % areas where to increase load
+%optns.load.loadIncreaseArea = 1; % areas where to increase load
+optns.bus.loadIncrease = 1:9; % buses with load increase for contingencies
 %% matpower options
 optns.mpopt = mpoption();
 
@@ -40,16 +44,18 @@ foptions.MaxIter = 100;
 
 %optns.mpopt.opf.violation = 1e-3;
 %% CHECK OPTIONS
-check_opf_options(optns);
+optns = check_opf_options(optns);
 
 %% SETUP MATPOWER CASE
 mpc = setup_mpc(optns);
 
-% convert to internal indexing
+%% CONTINGENCIES
+mpc = setup_contingencies(mpc,optns); 
+
+%% convert to internal indexing
 mpc = ext2int(mpc);
 
-%% CONTINGENCIES
-mpc.contingencies = setup_contingencies(optns);
+
 
 %% SETUPT OPTIMIZATION OBJECT
 om = setup_opf(mpc,optns);
@@ -58,7 +64,7 @@ om = setup_opf(mpc,optns);
 f_fcn = @(x)vscopf_f_maxPg(x, om);
 % constraint function
 [Ybus,Yf,Yt] = makeYbus(mpc);
-g_fcn = @(x)vscopf_g(x, om, Ybus, Yf, Yt, optns.mpopt);
+g_fcn = @(x)vscopf_g(x, om, optns.mpopt);
 
 [x0,LB,UB] = getv(om);
 
@@ -86,4 +92,4 @@ et = etime(clock,t0);
 [results.et, results.f, results.success] = deal(et,f,success);
 
 %fd = fopen('output.txt','at');
-printpf(results);
+printpf(results,optns.fileID);
