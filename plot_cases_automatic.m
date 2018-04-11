30ad97ce07912f38ddb29c0a3d7f3c89b852c8e5
Elis Nycander <elisnycander@gmail.com>
2018-04-11 12:35:03 +0200

    Added plot_runs_automatic which plots results for several runs

--- plot_cases_automatic.m
+++ plot_cases_automatic.m
@@ -2,154 +2,111 @@ close all;
 clear;
 
 saveAggregatePlots = 0;
-plotIndividualResults = 1;
+plotIndividualResults = 0;
+plotAggregateResults = 1;
 grey_plot = 0;
 
 %penetration = 0.8:0.005:0.995;
-%plot_run = 'run38';
-%plot_cases = {'run26_E1_PQ_P065','run26_E1_PQ_P07'};
-plot_cases = {'run43_E3_PQ_P0995'};
 
-if exist('plot_run','var') % plot whole run, overries plot_cases
-    
+%plot_run = 'run44';
+%plot_cases = {'run26_E1_PQ_P065','run26_E1_PQ_P07'};
+plot_cases = {'run44_E3_PQ_P099','run44_E3_PQ_P0995'};
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%
+
+if exist('plot_run','var') % plot whole run, overries plot_cases   
     D = dir(['data/' plot_run '*.mat']);
     N = length(D);
-
-    for i=1:N
-        
-        caseNames{i} =  strrep( strrep( strrep( D(i).name,[plot_run '_'],'' ), '_', ' '), '.mat','' );
-        
-        % load data
-        load(['data/' D(i).name]);
-        
-        f_values(i) = restab.f;
-        outmsg{i} = Output.message;
-        
-        % Total curtailment (for each scenario and case in MW)
-        curtail = table2array(restab.Curtail(:,setdiff( restab.Curtail.Properties.VariableNames,{'GEN','BUS'})));
-        curtailedTotal = sum(curtail,1);
-        curtailment_nom(i,:) = curtailedTotal;
-        
-        % Expected curtailment (for each case in MW)
-        curtailment_nom_exp(i) = sum(table2array(restab.ExpCurtail(3,:))); % same as f-values
-        
-        % Curtailment (for each scenario and case in MW)
-        wind = table2array(restab.Wind(:,setdiff(restab.Wind.Properties.VariableNames,{'GEN','BUS'})));
-        windTotal = sum(wind,1);
-        
-        curtailment_pc(i,:) = 100*curtailedTotal./windTotal;
-        
-        % Expected curtailment (for each case in %)
-        curtailment_pc_exp(i) = sum(curtailment_pc(i,:) .* table2array(restab.ExpCurtail(1,:)));
-        
-        % Transfer through corridors
-        transferNorthSouth(i,:) = table2array(restab.transferTo(1,setdiff( restab.transferFrom.Properties.VariableNames,{'CORRIDOR'})));
-        
-        
-        cols = restab.Ploss.Properties.VariableNames;
-        columns = {};
-        for ii=1:length(cols)
-            if contains(cols{ii},'MW')
-                columns{end+1} = cols{ii};
-            end
-        end
-        losses(i,:) = sum( table2array( restab.Ploss(:,columns) ), 1 );
-        
-        % sum total generation
-        Pg = table2array( restab.Pg( :,setdiff( restab.Pg.Properties.VariableNames, {'GEN','BUS','MIN','MAX'} ) ) );
-        ptot = sum( Pg, 1 );
-        
-        losses_pc(i,:) = losses(i,:) ./ ptot * 100;
-        
-        % net wind = wind - losses
-        pwind = sum( Pg(optns.gen.curtailableP,:),1 );
-        
-        
-        windnet(i,:) = pwind - losses(i,:);
-        
-        windscale(i) = pwind(1); % base case wind
-        
-        % successful optimization
-        success(i) = restab.success;     
-    end
-       
-else % plot cases in plot_cases
-    
+    looptype = 'run';
+else
     N = length(plot_cases);
-    %caseNames = cell(N,1);
+    looptype = 'cases';
+    plot_run = '';
+end
+
+penetration = [];
+for i=1:N
     
-    for i=1:N
-        
+    if isequal(looptype,'run')
+        iD = D(i);
+        caseNames{i} =  strrep( strrep( strrep( iD.name,[plot_run '_'],'' ), '_', ' '), '.mat','' );
+    elseif isequal(looptype,'cases')
         D = dir(['data/' plot_cases{i} '.mat']);
         if ~isempty(D)
-            
-            load(['data/' D(1).name]);
-            optns.plotGreyscale = grey_plot;
-            
-            if plotIndividualResults
-                figArray = plot_results_article(restab,optns);
-            end
-            
+            iD = D(1);
             caseNames{i} = strrep( strrep( D(1).name,[plot_cases{i} '_'],'' ), '_', ' ');
-            
-            f_values(i) = restab.f;
-            outmsg{i} = Output.message;
-            
-            % Total curtailment (for each scenario and case in MW)
-            curtail = table2array(restab.Curtail(:,setdiff( restab.Curtail.Properties.VariableNames,{'GEN','BUS'})));
-            curtailedTotal = sum(curtail,1);
-            curtailment_nom(i,:) = curtailedTotal;
-            
-            % Expected curtailment (for each case in MW)
-            curtailment_nom_exp(i) = sum(table2array(restab.ExpCurtail(3,:))); % same as f-values
-            
-            % Curtailment (for each scenario and case in MW)
-            wind = table2array(restab.Wind(:,setdiff(restab.Wind.Properties.VariableNames,{'GEN','BUS'})));
-            windTotal = sum(wind,1);
-            
-            curtailment_pc(i,:) = 100*curtailedTotal./windTotal;
-            
-            % Expected curtailment (for each case in %)
-            curtailment_pc_exp(i) = sum(curtailment_pc(i,:) .* table2array(restab.ExpCurtail(1,:)));
-            
-            % Transfer through corridors
-            transferNorthSouth(i,:) = table2array(restab.transferTo(1,setdiff( restab.transferFrom.Properties.VariableNames,{'CORRIDOR'})));
-            
-            
-            cols = restab.Ploss.Properties.VariableNames;
-            columns = {};
-            for ii=1:length(cols)
-                if contains(cols{ii},'MW')
-                    columns{end+1} = cols{ii};
-                end
-            end
-            losses(i,:) = sum( table2array( restab.Ploss(:,columns) ), 1 );
-            
-            % sum total generation
-            Pg = table2array( restab.Pg( :,setdiff( restab.Pg.Properties.VariableNames, {'GEN','BUS','MIN','MAX'} ) ) );
-            ptot = sum( Pg, 1 );
-            
-            losses_pc(i,:) = losses(i,:) ./ ptot * 100;
-            
-            % net wind = wind - losses
-            pwind = sum( Pg(optns.gen.curtailableP,:),1 );
-            
-            windnet(i,:) = pwind - losses(i,:);
-            
-            windscale(i) = pwind(1); % base case wind
-            
-            % successful optimization
-            success(i) = restab.success;
-            
         else
-            disp('No such file');
+            warning(['Cannot find ' plot_cases{i} '.mat']);
+            break;
         end
     end
     
-    plot_run = plot_cases{1};
-    if contains( plot_run,'_' )
-        plot_run = plot_run( 1:strfind( plot_run,'_' )-1 );
+    
+    %caseNames{i} =  strrep( strrep( strrep( iD.name,[plot_run '_'],'' ), '_', ' '), '.mat','' );
+    
+    % determine penetration
+    penetration = [penetration parse_penetration(iD.name)];
+    
+    % load data
+    load(['data/' iD.name]);
+    
+    if plotIndividualResults
+        figArray = plot_results(restab,optns);
     end
+    
+    f_values(i) = restab.f;
+    outmsg{i} = Output.message;
+    
+    % Total curtailment (for each scenario and case in MW)
+    curtail = table2array(restab.Curtail(:,setdiff( restab.Curtail.Properties.VariableNames,{'GEN','BUS'})));
+    curtailedTotal = sum(curtail,1);
+    curtailment_nom(i,:) = curtailedTotal;
+    
+    % Expected curtailment (for each case in MW)
+    curtailment_nom_exp(i) = sum(table2array(restab.ExpCurtail(3,:))); % same as f-values
+    
+    % Curtailment (for each scenario and case in MW)
+    wind = table2array(restab.Wind(:,setdiff(restab.Wind.Properties.VariableNames,{'GEN','BUS'})));
+    windTotal = sum(wind,1);
+    
+    curtailment_pc(i,:) = 100*curtailedTotal./windTotal;
+    
+    % Expected curtailment (for each case in %)
+    curtailment_pc_exp(i) = sum(curtailment_pc(i,:) .* table2array(restab.ExpCurtail(1,:)));
+    
+    % Transfer through corridors
+    transferNorthSouth(i,:) = table2array(restab.transferTo(1,setdiff( restab.transferFrom.Properties.VariableNames,{'CORRIDOR'})));
+    
+    
+    cols = restab.Ploss.Properties.VariableNames;
+    columns = {};
+    for ii=1:length(cols)
+        if contains(cols{ii},'MW')
+            columns{end+1} = cols{ii};
+        end
+    end
+    numtab = table2array(restab.Ploss(:,columns));
+    numtab( isnan(numtab) ) = 0;
+    losses(i,:) = sum( numtab, 1 );
+    
+    % sum total generation
+    Pg = table2array( restab.Pg( :,setdiff( restab.Pg.Properties.VariableNames, {'GEN','BUS','MIN','MAX'} ) ) );
+    Pg(isnan(Pg)) = 0;
+    ptot = sum( Pg, 1 );
+    
+    losses_pc(i,:) = losses(i,:) ./ ptot * 100;
+    
+    % net wind = wind - losses
+    pwind = sum( Pg(optns.gen.curtailableP,:),1 );
+    
+    
+    windnet(i,:) = pwind - losses(i,:);
+    
+    windscale(i) = pwind(1); % base case wind
+    
+    % successful optimization
+    success(i) = restab.success;
 end
 
 contingencyLabls = {};
@@ -158,125 +115,126 @@ for i=1:size(restab.ExpCurtail,2)
 end
 
 %% PLOTS
-% 
-% % objective function
-% F1 = figure;
-% bar(categorical(caseNames),curtailment_nom_exp.');
-% ylabel('Curtailment (MW)');
-% xlabel('Case');
-% title(['Total expected curtailment' '\newline' strrep(plot_run,'_',' ')])
-% 
-% 
-% F2 = figure;
-% bar(1:size(curtailment_nom,2),curtailment_nom.');
-% xlabel('Contingency scenario');
-% L2 = legend(caseNames);
-% L2.Position = [0.25 0.25 0 0];
-% ylabel('Curtailment (MW)');
-% title(['Total curtailment' '\newline' strrep(plot_run,'_',' ')])
-% 
-% F3 = figure;
-% bar(1:size(curtailment_pc,2),curtailment_pc.');
-% ylabel('Curtailment (%)');
-% L3 = legend(caseNames);
-% L3.Position = [0.25 0.25 0 0];
-% xlabel('Contingency scenario');
-% title(['Total curtailment' '\newline' strrep(plot_run,'_',' ')]);
-% 
-% F4 = figure;
-% bar(categorical(caseNames),curtailment_pc_exp.');
-% ylabel('Curtailment (%)');
-% xlabel('Case');
-% title(['Total expected curtailment' '\newline' strrep(plot_run,'_',' ')])
-% 
-% 
-% F5 = figure;
-% bar(1:size(transferNorthSouth,2),transferNorthSouth.');
-% ylabel('Transfer (MW)');
-% L5 = legend(caseNames);
-% L5.Position = [0.25 0.25 0 0];
-% xlabel('Contingency scenario');
-% title(['Transfer from North to Central' '\newline' strrep(plot_run,'_',' ')]);
-% figs = [F1 F2 F3 F4 F5];
-% 
-% if exist('penetration','var')
-%     F6 = figure;
-%     plot(penetration,curtailment_nom_exp,'*');
-%     grid on;
-%     xlabel('Penetration (%)');
-%     ylabel('Curtailment (MW)');
-%     title(['Total expected curtailment' '\newline' strrep(plot_run,'_',' ')])
-%     figs = [figs F6];
-%     
-% end
-% 
-% F7 = figure;
-% bar(categorical(caseNames),success.');
-% xlabel('Case')
-% title(['Successful optimization' '\newline' strrep(plot_run,'_',' ')])
-% 
-% figs = [figs F7];
-% if exist('penetration','var')
-%     figNames = {'CurTotMW','CurMW','CurPC','CurTotPC','Transfer','CurTotMW_xy','Success'};
-% else
-%     figNames = {'CurTotMW','CurMW','CurPC','CurTotPC','Transfer','Success'};
-% end
-% 
-% % bar(1:size(curtailment_nom,2),curtailment_nom.');
-% % xlabel('Contingency scenario');
-% % L2 = legend(caseNames);
-% % L2.Position = [0.25 0.25 0 0];
-% % ylabel('Curtailment (MW)');
-% % title(['Total curtailment' '\newline' strrep(plot_run,'_',' ')])
-% 
-% F8 = figure;
-% bar(1:size(losses,2),losses.');
-% xlabel('Contingency scenario');
-% ylabel('MW');
-% title(['Active power losses' '\newline' strrep(plot_run,'_',' ')]);
-% 
-% F9 = figure;
-% bar(1:size(losses_pc,2),losses_pc.');
-% xlabel('Contingency scenario');
-% ylabel('%');
-% title(['Active power losses' '\newline' strrep(plot_run,'_',' ')]);
-% 
-% figs = [figs F8 F9];
-% figNames{end+1} = 'LossMW';
-% figNames{end+1} = 'LossPC';
-% 
-% if exist('penetration','var') % plot losses as line plots
-%    
-%     
-%     F10 = figure;
-%     plot(penetration,losses);
-%     grid on;
-%     xlabel('Penetration (%)')
-%     ylabel('Losses (MW)')
-%     legend(contingencyLabls);
-%     
-%     F11 = figure; 
-%     plot(penetration,losses_pc);
-%     grid on
-%     xlabel('Penetration (%)');
-%     ylabel('Losses (MW)');
-%     legend(contingencyLabls);
-% 
-%     fig = [figs F10 F11];
-%     figNames{end+1} = 'LossXYMW';
-%     figNames{end+1} = 'LossXYPC';
-%     
-%     F12 = figure;
-%     plot(penetration,windnet);
-%     grid on;
-%     ylabel('P_{wind} - P_{loss} (MW)');
-%     legend(contingencyLabls);
-%     title('Net wind contribution');
-%     double_x_axis(F12,penetration,windscale)
-% end
-% 
-% 
-% 
+
+if plotAggregateResults
+    % objective function
+    F1 = figure;
+    bar(categorical(caseNames),curtailment_nom_exp.');
+    ylabel('Curtailment (MW)');
+    xlabel('Case');
+    title(['Total expected curtailment' '\newline' strrep(plot_run,'_',' ')])
+    
+    
+    F2 = figure;
+    bar(1:size(curtailment_nom,2),curtailment_nom.');
+    xlabel('Contingency scenario');
+    L2 = legend(caseNames);
+    L2.Position = [0.25 0.25 0 0];
+    ylabel('Curtailment (MW)');
+    title(['Total curtailment' '\newline' strrep(plot_run,'_',' ')])
+    
+    F3 = figure;
+    bar(1:size(curtailment_pc,2),curtailment_pc.');
+    ylabel('Curtailment (%)');
+    L3 = legend(caseNames);
+    L3.Position = [0.25 0.25 0 0];
+    xlabel('Contingency scenario');
+    title(['Total curtailment' '\newline' strrep(plot_run,'_',' ')]);
+    
+    F4 = figure;
+    bar(categorical(caseNames),curtailment_pc_exp.');
+    ylabel('Curtailment (%)');
+    xlabel('Case');
+    title(['Total expected curtailment' '\newline' strrep(plot_run,'_',' ')])
+    
+    
+    F5 = figure;
+    bar(1:size(transferNorthSouth,2),transferNorthSouth.');
+    ylabel('Transfer (MW)');
+    L5 = legend(caseNames);
+    L5.Position = [0.25 0.25 0 0];
+    xlabel('Contingency scenario');
+    title(['Transfer from North to Central' '\newline' strrep(plot_run,'_',' ')]);
+    figs = [F1 F2 F3 F4 F5];
+    
+    if exist('penetration','var')
+        F6 = figure;
+        plot(penetration,curtailment_nom_exp,'*');
+        grid on;
+        xlabel('Penetration (%)');
+        ylabel('Curtailment (MW)');
+        title(['Total expected curtailment' '\newline' strrep(plot_run,'_',' ')])
+        figs = [figs F6];
+        
+    end
+    
+    F7 = figure;
+    bar(categorical(caseNames),success.');
+    xlabel('Case')
+    title(['Successful optimization' '\newline' strrep(plot_run,'_',' ')])
+    
+    figs = [figs F7];
+    if exist('penetration','var')
+        figNames = {'CurTotMW','CurMW','CurPC','CurTotPC','Transfer','CurTotMW_xy','Success'};
+    else
+        figNames = {'CurTotMW','CurMW','CurPC','CurTotPC','Transfer','Success'};
+    end
+    
+    % bar(1:size(curtailment_nom,2),curtailment_nom.');
+    % xlabel('Contingency scenario');
+    % L2 = legend(caseNames);
+    % L2.Position = [0.25 0.25 0 0];
+    % ylabel('Curtailment (MW)');
+    % title(['Total curtailment' '\newline' strrep(plot_run,'_',' ')])
+    
+    F8 = figure;
+    bar(1:size(losses,2),losses.');
+    xlabel('Contingency scenario');
+    ylabel('MW');
+    title(['Active power losses' '\newline' strrep(plot_run,'_',' ')]);
+    
+    F9 = figure;
+    bar(1:size(losses_pc,2),losses_pc.');
+    xlabel('Contingency scenario');
+    ylabel('%');
+    title(['Active power losses' '\newline' strrep(plot_run,'_',' ')]);
+    
+    figs = [figs F8 F9];
+    figNames{end+1} = 'LossMW';
+    figNames{end+1} = 'LossPC';
+    
+    if exist('penetration','var') % plot losses as line plots
+        
+        
+        F10 = figure;
+        plot(penetration,losses);
+        grid on;
+        xlabel('Penetration (%)')
+        ylabel('Losses (MW)')
+        legend(contingencyLabls);
+        
+        F11 = figure;
+        plot(penetration,losses_pc);
+        grid on
+        xlabel('Penetration (%)');
+        ylabel('Losses (MW)');
+        legend(contingencyLabls);
+        
+        fig = [figs F10 F11];
+        figNames{end+1} = 'LossXYMW';
+        figNames{end+1} = 'LossXYPC';
+        
+        F12 = figure;
+        plot(penetration,windnet);
+        grid on;
+        ylabel('P_{wind} - P_{loss} (MW)');
+        legend(contingencyLabls);
+        title('Net wind contribution');
+        double_x_axis(F12,penetration,windscale)
+    end
+    
+end
+
 % if saveAggregatePlots
 %     for i=1:length(figs)
 %         thisFigureName = [plot_run '_' figNames{i}];
@@ -287,7 +245,7 @@ end
 
 % change font size
 % for i=1:length(figArray)
-%    
+%
 %    figArray(i).CurrentAxes.XLabel.FontSize = 14;
 %    figArray(i).CurrentAxes.YLabel.FontSize = 14;
 %    figArray(i);
@@ -295,5 +253,5 @@ end
 %    set(gca,'FontSize',14);
 %    yt = get(gca,'YTick');
 %    set(gca,'FontSize',14);
-%    % figs(i).CurrentAxes.XLabel.FontSize = 14; 
+%    % figs(i).CurrentAxes.XLabel.FontSize = 14;
 % end
---
+++ plot_runs_automatic.m
@@ -0,0 +1,171 @@
+%close all;
+clear;
+
+saveAggregatePlots = 0;
+plotIndividualResults = 1;
+
+runs = {'run41','run43','run42'};
+
+run_legends = {'C1, base case', 'C2, no wind at 1021', 'C3, wind PF \geq 0.9'};
+short_run_legends = {'C1','C2','C3'};
+run_markers = {'.','*','o'};
+run_marker_size = [6 4 4];
+run_colors = [0 0 0;0 0 0;0 0 0];
+
+penetration = 0.8:0.005:0.995;
+
+curtail_pc_all = [];
+windnet_all = [];
+transfer_all = [];
+curtail_all = [];
+losses_all = [];
+
+
+
+for j=1:length(runs)   
+
+    plot_run = runs{j};
+    
+    D = dir(['data/' plot_run '*.mat']);
+    N = length(D);
+    
+    caseNames = {};
+    f_values = [];
+    outmsg = {};
+    curtailment_nom = [];
+    curtailment_nom_exp = [];
+    curtailment_pc = [];
+    transferNorthSouth = [];
+    losses = [];
+    losses_pc = [];
+    windnet = [];
+    windscale = [];
+    success = [];
+
+
+    for i=1:N
+        
+        caseNames{i} =  strrep( strrep( strrep( D(i).name,[plot_run '_'],'' ), '_', ' '), '.mat','' );
+        
+        % load data
+        load(['data/' D(i).name]);
+        
+        f_values(i) = restab.f;
+        outmsg{i} = Output.message;
+        
+        % Total curtailment (for each scenario and case in MW)
+        curtail = table2array(restab.Curtail(:,setdiff( restab.Curtail.Properties.VariableNames,{'GEN','BUS'})));
+        curtailedTotal = sum(curtail,1);
+        curtailment_nom(i,:) = curtailedTotal;
+        
+        % Expected curtailment (for each case in MW)
+        curtailment_nom_exp(i) = sum(table2array(restab.ExpCurtail(3,:))); % same as f-values
+        
+        % Curtailment (for each scenario and case in MW)
+        wind = table2array(restab.Wind(:,setdiff(restab.Wind.Properties.VariableNames,{'GEN','BUS'})));
+        windTotal = sum(wind,1);
+        
+        curtailment_pc(i,:) = 100*curtailedTotal./windTotal;
+        
+        % Expected curtailment (for each case in %)
+        curtailment_pc_exp(i) = sum(curtailment_pc(i,:) .* table2array(restab.ExpCurtail(1,:)));
+        
+        % Transfer through corridors
+        transferNorthSouth(i,:) = abs(table2array(restab.transferTo(1,setdiff( restab.transferTo.Properties.VariableNames,{'CORRIDOR'}))));
+        
+        
+        cols = restab.Ploss.Properties.VariableNames;
+        columns = {};
+        for ii=1:length(cols)
+            if contains(cols{ii},'MW')
+                columns{end+1} = cols{ii};
+            end
+        end
+        tmp = table2array( restab.Ploss(:,columns ), 1);
+        tmp( isnan(tmp) ) = 0;
+        losses(i,:) = sum( tmp, 1 );
+        
+        % sum total generation
+        Pg = table2array( restab.Pg( :,setdiff( restab.Pg.Properties.VariableNames, {'GEN','BUS','MIN','MAX'} ) ) );
+        Pg( isnan(Pg) ) = 0;
+        ptot = sum( Pg, 1 );
+        
+        losses_pc(i,:) = losses(i,:) ./ ptot * 100;
+        
+        % net wind = wind - losses
+        pwind = sum( Pg(optns.gen.curtailableP,:),1 );
+        
+        
+        windnet(i,:) = pwind - losses(i,:);
+        
+        windscale(i) = pwind(1); % base case wind
+        
+        % successful optimization
+        success(i) = restab.success;     
+    end
+    % save desired variables
+    curtail_all = [curtail_all curtailment_nom(:,2)];
+    curtail_pc_all = [curtail_pc_all curtailment_pc(:,2)];
+    windnet_all = [windnet_all windnet(:,2)];
+    transfer_all = [transfer_all transferNorthSouth];
+    losses_all = [losses_all losses];
+    
+
+end
+
+    % create legend
+    run_legends_x_contingencies = {};
+    ij = 1;
+    for i=1:length(runs)
+        N = size(restab.ExpCurtail,2);
+        for j=1:N
+            run_legends_x_contingencies{ij} = [short_run_legends{i} ', S' num2str(j)];
+            ij = ij+1;
+        end
+    end
+
+%% PLOTS,penetration,windscale)
+
+F13 = figure;
+PH13 = plot(penetration, curtail_all); % only high-wind scenarios
+ylabel('Curtailment (MW)');
+L13 = legend(run_legends);
+grid on;
+double_x_axis(F13,penetration,windscale);
+change_markers(PH13,run_markers,run_marker_size);
+%tight_axis(F13);
+
+F14 = figure;
+PH14 = plot(penetration,windnet_all);
+grid on;
+ylabel('P_{wind} - P_{loss} (MW)');
+legend(run_legends);
+grid on;
+%axis tight
+%ylim([3500 4500]);
+double_x_axis(F14,penetration,windscale);
+change_markers(PH14,run_markers,run_marker_size);
+%tight_axis(F14);
+
+F15 = figure;
+PH15 = plot(penetration,transfer_all);
+grid on;
+ylabel('Transfer (MW)');
+legend(run_legends_x_contingencies);
+double_x_axis(F15,penetration,windscale);
+change_markers(PH15,run_markers,run_marker_size);
+
+F16 = figure;
+PH16 = plot(penetration,losses_all);
+grid on;
+ylabel('Losses (MW)');
+legend(run_legends_x_contingencies);
+double_x_axis(F16,penetration,windscale);
+change_markers(PH16,run_markers,run_marker_size);
+% if saveAggregatePlots
+%     for i=1:length(figs)
+%         thisFigureName = [plot_run '_' figNames{i}];
+%         saveas(figs(i),['figures/runs/' thisFigureName '.fig']);
+%         saveas(figs(i),['figures/runs/' thisFigureName '.png']);
+%     end
+% end
